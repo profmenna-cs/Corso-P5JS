9f0be83425726a210917eb6e3d5934b9eed48190
Gabo <gabo@moldiscovery.com>
2025-09-12 17:36:44 +0200

    added missing

---
+++ p5js/4/star.js
@@ -0,0 +1,12 @@
+let x_pos = 200;
+let y_pos = 200;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(29, 40, 115);
+    fill(255, 242, 0);
+    ellipse(x_pos, y_pos, 10, 10);
+};
